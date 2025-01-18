7ffa5aca1db9df90a5fe68d435e3bd2447cdc359
bhuvanvarma <bhuvan.varma123@gmail.com>
2025-01-18 22:26:46 +0530

    learned methods

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-Vue.createApp({
+const vm = Vue.createApp({
     data(){
         return {
-            firstName: 'Bhuvan'
+            firstName: 'Bhuvan',
+            lastName: 'Varma'
+        }
+    },
+    methods: {
+        fullName(){
+            return `${this.firstName} ${this.lastName.toUpperCase()}`
         }
     }
 }).mount('#app')
+
+setTimeout(() => {
+    vm.firstName = "Sonu"
+}, 2000)
