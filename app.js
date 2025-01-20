4513345f17c2422dcbbb0ba6f825d7561ce153d0
BhuvanVarma <bhuvan.varma123@gmail.com>
2025-01-20 17:18:32 +0530

    listening to events

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const vm = Vue.createApp({
         return {
             firstName: 'Bhuvan',
             lastName: 'Varma',
-            URL: "https://www.google.com"
+            URL: "https://www.google.com",
+            raw_url: '<a href="https://www.google.com">Google</a>',
+            age: 23
         }
     },
     methods: {
         fullName(){
             return `${this.firstName} ${this.lastName.toUpperCase()}`
+        },
+        increment(){
+            this.age++
+        },
+        updateLastName(event){
+            this.lastName = event.target.value
         }
     }
 }).mount('#app')
